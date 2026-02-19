0df50e7b0169e8f8d82c39751433309319d98c5d
EgorKarbyshev1 <egorkarbysev81@gmail.com>
2026-02-19 18:47:46 +0300

    .

--- propositions/operators.py
+++ propositions/operators.py
@@ -1,3 +1,114 @@
+# This file is part of the materials accompanying the book
+# "Mathematical Logic through Python" by Gonczarowski and Nisan,
+# Cambridge University Press. Book site: www.LogicThruPython.org
+# (c) Yannai A. Gonczarowski and Noam Nisan, 2017-2022
+# File name: propositions/operators.py
+
+"""Syntactic conversion of propositional formulas to use only specific sets of
+operators."""
+
+from propositions.syntax import *
+from propositions.semantics import *
+
+def to_not_and_or(formula: Formula) -> Formula:
+    """Syntactically converts the given formula to an equivalent formula that
+    contains no constants or operators beyond ``'~'``, ``'&'``, and ``'|'``.
+
+    Parameters:
+        formula: formula to convert.
+
+    Returns:
+        A formula that has the same truth table as the given formula, but
+        contains no constants or operators beyond ``'~'``, ``'&'``, and
+        ``'|'``.
+    """
+    if is_constant(formula.root):
+        if formula.root == 'T':
+            return Formula('|', Formula('p'), Formula('~', Formula('p')))
+        else:
+            return Formula('&', Formula('p'), Formula('~', Formula('p')))
+            
+    if is_variable(formula.root):
+        return Formula(formula.root)
+        
+    if is_unary(formula.root):
+        return Formula('~', to_not_and_or(formula.first))
+        
+    if is_binary(formula.root):
+        first = to_not_and_or(formula.first)
+        second = to_not_and_or(formula.second)
+        
+        if formula.root == '&':
+            return Formula('&', first, second)
+        if formula.root == '|':
+            return Formula('|', first, second)
+        if formula.root == '->':
+            return Formula('|', Formula('~', first), second)
+        if formula.root == '+':
+            return Formula('|', 
+                          Formula('&', first, Formula('~', second)),
+                          Formula('&', Formula('~', first), second))
+        if formula.root == '<->':
+            return Formula('|',
+                          Formula('&', first, second),
+                          Formula('&', Formula('~', first), Formula('~', second)))
+        if formula.root == '-&':
+            return Formula('~', Formula('&', first, second))
+        if formula.root == '-|':
+            return Formula('~', Formula('|', first, second))
+
+def to_not_and(formula: Formula) -> Formula:
+    """Syntactically converts the given formula to an equivalent formula that
+    contains no constants or operators beyond ``'~'`` and ``'&'``.
+
+    Parameters:
+        formula: formula to convert.
+
+    Returns:
+        A formula that has the same truth table as the given formula, but
+        contains no constants or operators beyond ``'~'`` and ``'&'``.
+    """
+    not_and_or = to_not_and_or(formula)
+    
+    if is_constant(not_and_or.root):
+        return not_and_or
+    if is_variable(not_and_or.root):
+        return not_and_or
+    if is_unary(not_and_or.root):
+        return Formula('~', to_not_and(not_and_or.first))
+    if not_and_or.root == '&':
+        return Formula('&', to_not_and(not_and_or.first), 
+                      to_not_and(not_and_or.second))
+    if not_and_or.root == '|':
+        return Formula('~', 
+                      Formula('&', 
+                             Formula('~', to_not_and(not_and_or.first)),
+                             Formula('~', to_not_and(not_and_or.second))))
+
+def to_nand(formula: Formula) -> Formula:
+    """Syntactically converts the given formula to an equivalent formula that
+    contains no constants or operators beyond ``'-&'``.
+
+    Parameters:
+        formula: formula to convert.
+
+    Returns:
+        A formula that has the same truth table as the given formula, but
+        contains no constants or operators beyond ``'-&'``.
+    """
+    not_and = to_not_and(formula)
+    
+    if is_variable(not_and.root):
+        return not_and
+    if is_unary(not_and.root):
+        arg = to_nand(not_and.first)
+        return Formula('-&', arg, arg)
+    if not_and.root == '&':
+        left = to_nand(not_and.first)
+        right = to_nand(not_and.second)
+        nand = Formula('-&', left, right)
+        return Formula('-&', nand, nand)
+
 def to_implies_not(formula: Formula) -> Formula:
     """Syntactically converts the given formula to an equivalent formula that
     contains no constants or operators beyond ``'->'`` and ``'~'``.
@@ -12,7 +123,7 @@ def to_implies_not(formula: Formula) -> Formula:
     if is_constant(formula.root):
         if formula.root == 'T':
             return Formula('->', Formula('p'), Formula('p'))
-        else:  # 'F'
+        else:
             return Formula('~', Formula('->', Formula('p'), Formula('p')))
             
     if is_variable(formula.root):
@@ -26,35 +137,22 @@ def to_implies_not(formula: Formula) -> Formula:
         second = to_implies_not(formula.second)
         
         if formula.root == '&':
-            # (a & b) = ~(a -> ~b)
             return Formula('~', Formula('->', first, Formula('~', second)))
-            
         if formula.root == '|':
-            # (a | b) = (~a -> b)
             return Formula('->', Formula('~', first), second)
-            
         if formula.root == '->':
-            # (a -> b) = a -> b
             return Formula('->', first, second)
-            
         if formula.root == '+':
-            # (a + b) = ~((a -> ~b) -> ~(~a -> b))
             left = Formula('->', first, Formula('~', second))
-            right = Formula('->', Formula('~', first), second)
+            right = Formula('->', second, Formula('~', first))
             return Formula('~', Formula('->', left, Formula('~', right)))
-            
         if formula.root == '<->':
-            # (a <-> b) = ~((a -> b) -> ~(b -> a))
             left = Formula('->', first, second)
             right = Formula('->', second, first)
             return Formula('~', Formula('->', left, Formula('~', right)))
-            
         if formula.root == '-&':
-            # (a -& b) = (a -> ~b)
             return Formula('->', first, Formula('~', second))
-            
         if formula.root == '-|':
-            # (a -| b) = ~(~a -> b)
             return Formula('~', Formula('->', Formula('~', first), second))
 
 def to_implies_false(formula: Formula) -> Formula:
@@ -71,7 +169,7 @@ def to_implies_false(formula: Formula) -> Formula:
     if is_constant(formula.root):
         if formula.root == 'T':
             return Formula('->', Formula('p'), Formula('p'))
-        else:  # 'F'
+        else:
             return Formula('F')
             
     if is_variable(formula.root):
@@ -79,47 +177,33 @@ def to_implies_false(formula: Formula) -> Formula:
         
     if is_unary(formula.root):
         if formula.root == '~':
-            # ~a = (a -> F)
             return Formula('->', to_implies_false(formula.first), Formula('F'))
-        return to_implies_false(formula.first)  # для других унарных (их нет)
+        return to_implies_false(formula.first)
         
     if is_binary(formula.root):
         first = to_implies_false(formula.first)
         second = to_implies_false(formula.second)
         
         if formula.root == '&':
-            # (a & b) = (a -> (b -> F)) -> F
             return Formula('->', 
                           Formula('->', first, 
                                  Formula('->', second, Formula('F'))),
                           Formula('F'))
-            
         if formula.root == '|':
-            # (a | b) = (a -> F) -> b
             return Formula('->', Formula('->', first, Formula('F')), second)
-            
         if formula.root == '->':
-            # (a -> b) = a -> b
             return Formula('->', first, second)
-            
         if formula.root == '+':
-            # (a + b) = (a -> (b -> F)) -> (b -> (a -> F)) -> F
             left = Formula('->', first, Formula('->', second, Formula('F')))
             right = Formula('->', second, Formula('->', first, Formula('F')))
             return Formula('->', left, Formula('->', right, Formula('F')))
-            
         if formula.root == '<->':
-            # (a <-> b) = (a -> b) -> ((b -> a) -> F) -> F
             left = Formula('->', first, second)
             right = Formula('->', second, first)
             return Formula('->', left, Formula('->', right, Formula('F')))
-            
         if formula.root == '-&':
-            # (a -& b) = a -> (b -> F)
             return Formula('->', first, Formula('->', second, Formula('F')))
-            
         if formula.root == '-|':
-            # (a -| b) = (a -> F) -> (b -> F)
             return Formula('->', 
                           Formula('->', first, Formula('F')),
                           Formula('->', second, Formula('F')))
